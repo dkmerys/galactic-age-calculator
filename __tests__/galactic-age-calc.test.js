bea1bde3d77fc7a85b4203ac817b1c91f5053a83
Daniel Merys <dkmerys@gmail.com>
2020-04-17 08:44:45 -0700

    add test to create new calculator object

---
+++ __tests__/galactic-age-calc.test.js
@@ -0,0 +1,7 @@
+describe ('Galactic-calculator', () => {
+
+  test('should create new Galactic-calculator object', () => {
+    let calculator = new Galactic-calculator;
+    expect(calculator).toEqual({})
+  })
+})
